e6082328702c5aeb19acc2fc052580633f256960
krypty <gmarigliano93@gmail.com>
2015-11-21 18:40:18 +0100

    add login dialog

---
+++ LoginDialog.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'res/gui/login_dialog.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 300)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.le_access_key_id = QtWidgets.QLineEdit(Dialog)
+        self.le_access_key_id.setObjectName("le_access_key_id")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.le_access_key_id)
+        self.le_secret_key_id = QtWidgets.QLineEdit(Dialog)
+        self.le_secret_key_id.setObjectName("le_secret_key_id")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.le_secret_key_id)
+        self.cbx_region = QtWidgets.QComboBox(Dialog)
+        self.cbx_region.setObjectName("cbx_region")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.cbx_region)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setObjectName("label_3")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_3)
+        self.verticalLayout.addLayout(self.formLayout)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.btn_login = QtWidgets.QPushButton(Dialog)
+        self.btn_login.setObjectName("btn_login")
+        self.horizontalLayout.addWidget(self.btn_login)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Secret access key: "))
+        self.label_2.setText(_translate("Dialog", "Access key id: "))
+        self.label_3.setText(_translate("Dialog", "Region"))
+        self.btn_login.setText(_translate("Dialog", "Login"))
+
--- Main.py
+++ Main.py
@@ -1,6 +1,17 @@
+import configparser
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import boto.ec2
+import boto.exception
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+from LoginDialog import Ui_Dialog
 from mainwindow import Ui_MainWindow
+
+
+def read_config(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    return config
 
 
 # inflate the mainwindow.py gui (generated from res/gui/mainwindow.ui)
@@ -19,13 +30,62 @@ class MainWindow(QMainWindow):
         self.ui.btnRefreshInstances.setText(args)
 
 
+# inflate the mainwindow.py gui (generated from res/gui/mainwindow.ui)
+class LoginDialog(QDialog):
+    def __init__(self):
+        super(QDialog, self).__init__()
+
+        # Set up the user interface from Designer.
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.ui.cbx_region.addItems([r.name for r in boto.ec2.regions()])
+
+        # Signals and Slots
+        self.ui.btn_login.clicked.connect(self._btn_login_clicked)
+
+    def _btn_login_clicked(self):
+
+        # here we ask the user to give us its credentials
+        access_key_id = self.ui.le_access_key_id.text()
+        secret_key_id = self.ui.le_secret_key_id.text()
+
+        # note: for us select: eu-central-1
+        region = self.ui.cbx_region.itemText(self.ui.cbx_region.currentIndex())
+
+        # or we can imagine that if a file credentials.ini is present we use it directly
+        # config = read_config(r'credentials/credentials.ini')
+        # access_key_id = config["auth"]["ACCESS_KEY"]
+        # secret_key_id = config["auth"]["SECRET_KEY"]
+
+        print("access_key_id:", access_key_id)
+        print("secret_key_id:", secret_key_id)
+        print("region:", region)
+
+        try:
+            conn = boto.ec2.connect_to_region(region,
+                                              aws_access_key_id=access_key_id,
+                                              aws_secret_access_key=secret_key_id)
+
+            sc_groups = conn.get_all_security_groups()
+            print(sc_groups)
+
+        except boto.exception.EC2ResponseError:
+            msg = "Impossible to log in using these credentials.\n" \
+                  "Please check your credentials and regions"
+
+            QMessageBox.critical(self, "Cannot login!", msg)
+
+
 # Main Function
 if __name__ == '__main__':
     # Create main app
     myApp = QApplication(sys.argv)
 
-    mv = MainWindow()
-    mv.show()
+    login_dialog = LoginDialog()
+    login_dialog.show()
+    # mv = MainWindow()
+    # mv.show()
 
     # Execute the Application and Exit
     myApp.exec_()
