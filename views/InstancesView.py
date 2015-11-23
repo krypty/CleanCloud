5ee86eb2fda87bfebde63e965373472e70b619f1
krypty <gmarigliano93@gmail.com>
2015-11-23 21:42:46 +0100

    show InstanceView in MainWindow and add refresh and delete methods in InstanceView

--- views/InstancesView.py
+++ views/InstancesView.py
@@ -1,4 +1,6 @@
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QWidget
+
 from gen.gui.instances_view import Ui_Form
 from controllers.InstanceController import InstanceController
 
@@ -14,14 +16,53 @@ class InstancesView(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
-        self._refresh_gui()
-
         # Signals and Slots
-        # self.ui.btn.clicked.connect(lambda: self._handle_button("lala"))
+        self.ui.btn_refresh.clicked.connect(self._refresh_gui)
+        self.ui.lw_instances.itemSelectionChanged.connect(self._on_item_selection_changed)
+        self.ui.btn_delete.clicked.connect(self._on_btn_delete_clicked)
+
+        self._refresh_gui()
 
     def _handle_button(self, args):
         self.ui.btn_refresh.setText(args)
 
     def _refresh_gui(self):
+        self.controller.refresh()
         names = self.controller.get_simple_names()
-        self.ui.listWidget.addItems(names)
+        print("refresh_gui", names)
+        self.ui.lw_instances.clear()
+        self.ui.lw_instances.addItems(names)
+
+    def _on_btn_delete_clicked(self):
+        selected_items_str = [item.text() for item in self.ui.lw_instances.selectedItems()]
+        selected_items = self.controller.get_selected_items(selected_items_str)
+        self.controller.delete(selected_items)
+
+        self._refresh_gui()
+
+    def _on_item_selection_changed(self):
+        self.ui.lbl_details_data.clear()
+
+        selected_item_count = len(self.ui.lw_instances.selectedItems())
+        self._update_item_count_in_gui(selected_item_count)
+
+        if selected_item_count == 1:
+            selected_item_str = self.ui.lw_instances.selectedItems()[0].text()
+            selected_items = self.controller.get_selected_items([selected_item_str])
+            item_details = self.controller.get_item_details(selected_items[0])
+
+            # TODO better string formatting
+            item_details_str = ""
+            for key, value in item_details.items():
+                item_details_str += "%s, %s\n" % (key, value)
+
+            self.ui.lbl_details_data.setText(item_details_str)
+
+    def _update_item_count_in_gui(self, count):
+        if count > 0:
+            self.ui.btn_delete.setText("Delete (%s)" % count)
+            self.ui.btn_stop.setText("Stop (%s)" % count)
+
+        else:
+            self.ui.btn_delete.setText("Delete")
+            self.ui.btn_stop.setText("Stop")
--- views/MainWindow.py
+++ views/MainWindow.py
@@ -1,25 +1,42 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QListView
 from gen.gui.mainwindow import Ui_MainWindow
+from views.InstancesView import InstancesView
 
 
 # inflate the mainwindow.py gui (generated from res/gui/mainwindow.ui)
+
+
 class MainWindow(QMainWindow):
     def __init__(self, conn):
         super(MainWindow, self).__init__()
+
+        # private attributes
+        self._conn = conn
 
         # Set up the user interface from Designer.
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # private attributes
-        self._conn = conn
+        self._add_blocks_into_grid()
 
         # debuging purpose
         # sc_groups = conn.get_all_security_groups()
         # print(sc_groups)
 
         # Signals and Slots
-        self.ui.btn_refresh.clicked.connect(lambda: self._handle_button("lala"))
+        # TODO add signal/slots for delete all and refresh all
+        self.ui.btn_delete_all.clicked.connect(lambda: self._handle_button("lala"))
+        self.ui.btn_refresh_all.clicked.connect(lambda: self._handle_button("lala"))
 
     def _handle_button(self, args):
-        self.ui.btn_refresh.setText(args)
+        self.ui.btn_refresh_all.setText(args)
+
+    def _add_blocks_into_grid(self):
+        instances_view = InstancesView(conn=self._conn)
+
+        self.ui.gl_blocks.addWidget(instances_view, 0, 0)
+
+        # TODO add reals views
+        self.ui.gl_blocks.addWidget(QListView(), 0, 1)
+        self.ui.gl_blocks.addWidget(QListView(), 1, 0)
+        self.ui.gl_blocks.addWidget(QListView(), 1, 1)
